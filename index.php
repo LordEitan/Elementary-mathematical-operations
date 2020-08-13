3b355d82d1689344b3788989acfd730c383ecb8c
LordEitan <mestechko11@outlook.com>
2020-08-13 17:31:35 +0300

    Simple math operation

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+    $a = 3; // task # 1 to assign the value a and display on the screen
+    echo "$a<br/>";
+    echo "<br>";
+    $a1 = 10;
+    $b = 2;
+    $summ = $a1 + $b;
+    $diff = $a1 - $b;
+    $mult = $a1 * $b;
+    $dill = $a1/$b;    // task # 2 create two variables and perform mathematical operations
+    echo "$summ<br/>";
+    echo "<br>";
+    echo "$diff<br/>";
+    echo "<br>";
+    echo "$mult<br/>";
+    echo "<br>";
+    echo "$dill<br/>";
+    echo "<br>";
+
+    $c = 15;
+    $d = 2;
+    $result = $c + $d; // task # 3 in which you need to create variables with c and d, produce the sum and output to the variable result
+    echo "$result<br/>";
+    echo "<br>";
+
+    $a2 = 10;
+    $b2 = 2;
+    $c2 = 5;
+    $summ2 = $a2 + $b2 + $c2; // task # 4 in which you need to create 3 variables and produce the sum between them
+    echo "$summ2<br/>";
+    echo "<br>";
+
+    $a3 = 17;
+    $b3 = 10;
+    $c3 = $a3 - $b3;
+    $d2 = 7;
+    $result1 = $c3 + $d2; // task # 5 in which it was necessary to first create 2 variables a3 and b3, and subtract the variable b3 from a3, then assign the difference to the variable c3, then create the variable d2, add between c3 and d2 and display the result of the variable result1
+    echo "$result1<br/>";
+    echo "<br>";
+
+    $text = 'Привет, Мир!';  // task # 6 in which it was necessary to create a variable with text output to the screen
+    echo "$text<br/>";
+    echo "<br>";
+
+    $text1 = 'Привет, ';
+    $text2 = 'Мир!';
+    $summ3 = $text1 . $text2;  // task # 7 in which it was necessary to create two variables and thus add as if to concatenate or concatenate two strings using the concatenation operator ('.'), which returns a string representing the concatenation of the left and right arguments.
+    echo "$summ3<br/>";
+
